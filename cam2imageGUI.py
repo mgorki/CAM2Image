7c64fab8d7201cf36dab7d886ef5b79a7260bebc
Michael Gorki <michael.gorki@gmx.de>
2023-07-24 11:53:53 +0200

    cleanup

--- cam2imageGUI.py
+++ cam2imageGUI.py
@@ -222,7 +222,7 @@ def completion(operation=None):
     ## Create an event loop
     while True:
         event, values = window.read()
-        # End program if user closes window or presses the OK button
+        ## End program if user closes window or presses the OK button
         if event == "OK" or event == sg.WIN_CLOSED:
             break
 
